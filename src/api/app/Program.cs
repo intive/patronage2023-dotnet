ce390787158bb50c245d6b8833430a15f59861ea
Michał Szczepaniak <miher13@wp.pl>
2023-03-10 08:29:58 +0100

    Added request/response information

--- src/api/app/Program.cs
+++ src/api/app/Program.cs
@@ -1,11 +1,16 @@
 using Intive.Patronage2023.Modules.Example.Api;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddExampleModule();
+builder.Services.AddHttpLogging(logging =>
+{
+	logging.LoggingFields = HttpLoggingFields.All;
+});
 
 var app = builder.Build();
 
 app.UseExampleModule();
-
+app.UseHttpLogging();
 app.Run();
