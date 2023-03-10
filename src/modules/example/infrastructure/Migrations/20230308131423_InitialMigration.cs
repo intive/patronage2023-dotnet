802797f410bdf70c2f9f1470f00a073e6911a339
Aleksandra Lipert <ola.lipert@gmail.com>
2023-03-10 13:22:55 +0100

    [PATRO23-47] Add initial migration.

---
+++ src/modules/example/infrastructure/Migrations/20230308131423_InitialMigration.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intive.Patronage2023.Modules.Example.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExampleAggregates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExampleAggregates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExampleAggregates");
+        }
+    }
+}
