ba361f4fe0b64eb89332e83cc0a1d192dd934b18
KlaudiaGajocha <klaudiagajocha99@gmail.com>
2023-03-12 17:09:20 +0100

    feature/PATRO23-50-Add-Console/Application-Insights-logging

--- src/api/app/Program.cs
+++ src/api/app/Program.cs
@@ -2,9 +2,32 @@ using Intive.Patronage2023.Modules.Example.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Set of logging providers added by WebApplication.CreateBuilder
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+// Configure JSON logging to the console.
+builder.Logging.AddJsonConsole();
+builder.Services.AddRazorPages();
+
 builder.Services.AddExampleModule();
 
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.UseExampleModule();
 
