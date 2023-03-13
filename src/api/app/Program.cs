3db76be79337c71c2c7625e661b36d2e2bf168cc
Michał Szczepaniak <miher13@wp.pl>
2023-03-13 16:49:50 +0100

    Resolves problem with merging

--- src/api/app/Program.cs
+++ src/api/app/Program.cs
@@ -1,4 +1,5 @@
 using Intive.Patronage2023.Modules.Example.Api;
+using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,34 @@ builder.Services.AddHttpLogging(logging =>
 	logging.ResponseBodyLogLimit = 4096;
 });
 
+builder.Services.AddSwaggerGen(options =>
+{
+	options.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Version = "v1",
+		Title = "Intive Patronage2023 Some Title Api",
+		Description = "An ASP.NET Core Web API for managing bills and more",
+	});
+
+	// Searching for all files with ".Api.xml" suffix, which should be api docs,
+	// in build directory and attach them to swagger
+	var xmlFiles = Directory.GetFiles(
+		AppContext.BaseDirectory,
+		"*.Api.xml",
+		SearchOption.TopDirectoryOnly).ToList();
+	xmlFiles.ForEach(xmlFile => options.IncludeXmlComments(xmlFile));
+});
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
 app.UseExampleModule();
 app.UseHttpLogging();
 app.Run();
