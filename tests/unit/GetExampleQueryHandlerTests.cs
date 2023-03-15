7083f95400b865a60c471899ba88d39ec658c611
Łukasz Winowicz <lwinowicz@gmail.com>
2023-03-15 17:09:00 +0100

    add summary to GETExampleQueryHandlerTests

--- tests/unit/GetExampleQueryHandlerTests.cs
+++ tests/unit/GetExampleQueryHandlerTests.cs
@@ -4,13 +4,16 @@ using Intive.Patronage2023.Modules.Example.Application.Example.GettingExamples;
 namespace Intive.Patronage2023.Example.Tests
 {
 	/// <summary>
-	/// Test that check if the method throws a "NotImplementedException" when called,
-	/// indicating that the implementation of the method is not yet complete.
+	/// Test that checks the behavior of a query handler class "HandleGetExamples".
 	/// </summary>
 	public class GetExampleQueryHandlerTests
 	{
+		/// <summary>
+		/// Test that check if the method throws a "NotImplementedException" when called,
+		/// indicating that the implementation of the method is not yet complete.
+		/// </summary>
 		[Fact]
-		public async Task Handle_WhenCalled_ReturnsPagedListExampleInfo()
+		public void Handle_WhenCalled_ReturnsPagedListExampleInfo()
 		{
 			// Arrange
 			var query = new GetExamples();
