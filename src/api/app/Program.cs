fc45193671dd46273eaf575cb31ce01cf17773a3
Aleksandra Lipert <ola.lipert@gmail.com>
2023-03-15 20:15:01 +0100

    Added a line break in Program.cs.

--- src/api/app/Program.cs
+++ src/api/app/Program.cs
@@ -1,6 +1,7 @@
 using Intive.Patronage2023.Modules.Example.Api;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddExampleModule(builder.Configuration);
 
 var app = builder.Build();
