02348bd7e8f72b61ac28b70be1d5d3e3b5db229c
Aleksandra Lipert <ola.lipert@gmail.com>
2023-03-16 07:46:36 +0100

    Added example columns configuration.

--- src/modules/example/infrastructure/Migrations/20230316063936_AddExColumnsConfiguration.Designer.cs
+++ src/modules/example/infrastructure/Migrations/20230316063936_AddExColumnsConfiguration.Designer.cs
@@ -2,15 +2,18 @@
 using Intive.Patronage2023.Modules.Example.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Intive.Patronage2023.Modules.Example.Infrastructure.Migrations
 {
 	[DbContext(typeof(ExampleDbContext))]
-    partial class ExampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230316063936_AddExColumnsConfiguration")]
+    partial class AddExColumnsConfiguration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
