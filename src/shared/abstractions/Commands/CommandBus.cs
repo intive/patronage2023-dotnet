8564ed4f1ef9e1b18a343951307a3be93d18ef82
alanpawski <A.Pawski.AP@gmail.com>
2023-03-19 13:25:07 +0100

    PATRO23-48: Added CommandBus and QueryBus implementation using MediatR

---
+++ src/shared/abstractions/Commands/CommandBus.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Intive.Patronage2023.Shared.Abstractions.Commands
+{
+	public class CommandBus
+	{
+		public CommandBus()
+		{
+		}
+	}
+}
+
---
+++ src/shared/abstractions/Queries/QueryBus.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Intive.Patronage2023.Shared.Abstractions.Queries
+{
+	public class QueryBus
+	{
+		public QueryBus()
+		{
+		}
+	}
+}
+
