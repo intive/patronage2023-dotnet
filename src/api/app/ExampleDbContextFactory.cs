804d904578c4f8837601fa6f90b0f74382b85efd
DawidBski <dawibski@gmail.com>
2023-03-23 00:42:39 +0100

    [PATRO23-154] Add Example Db Contect Factory file

---
+++ src/api/app/ExampleDbContextFactory.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using Intive.Patronage2023.Modules.Example.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Intive.Patronage2023.Api
+{
+	/// <summary>
+	///  provides a new instance of a DbContext for use in design-time scenarios, such as running migrations.
+	/// </summary>
+	public class ExampleDbContextFactory : IDesignTimeDbContextFactory<ExampleDbContext>
+	{
+		/// <inheritdoc/>
+		public ExampleDbContext CreateDbContext(string[] args)
+		{
+			IConfigurationRoot configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			var optionsBuilder = new DbContextOptionsBuilder<ExampleDbContext>();
+			optionsBuilder.UseSqlServer(configuration.GetConnectionString("DockerDb"));
+
+			return new ExampleDbContext(optionsBuilder.Options);
+		}
+	}
+}
