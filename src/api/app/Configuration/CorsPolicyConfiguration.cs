fa46ec432f41beeb327c4294319d84e2a423bb55
Łukasz Winowicz <lwinowicz@gmail.com>
2023-03-24 12:53:02 +0100

    [PATRO23-105] add summary

--- src/api/app/Configuration/CorsPolicyConfiguration.cs
+++ src/api/app/Configuration/CorsPolicyConfiguration.cs
@@ -4,17 +4,17 @@ using Microsoft.Extensions.Configuration;
 namespace Intive.Patronage2023.Api.Configuration
 {
 	/// <summary>
-	/// text.
+	/// Static class CorsPolicyConfiguration.
 	/// </summary>
 	public static class CorsPolicyConfiguration
 	{
 		/// <summary>
-		/// text.
+		/// Eetension method named that extends the IServiceCollection interface, that add Cross-Origin Resource Sharing (CORS) middleware to an application.
 		/// </summary>
-		/// <param name="services">text1.</param>
-		/// <param name="configuration">text2.</param>
-		/// <param name="corsPolicyName">text3.</param>
-		/// <returns>text4.</returns>
+		/// <param name="services">IServiceCollection object.</param>
+		/// <param name="configuration">IConfiguration object.</param>
+		/// <param name="corsPolicyName">Name of the CORS policy.</param>
+		/// <returns>Returns <ref name="IServiceCollection"/>IServiceCollection.</returns>
 		public static IServiceCollection AddCors(
 			this IServiceCollection services, IConfiguration configuration, string corsPolicyName) => services.AddCors(options =>
 			{
