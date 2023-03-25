93572e55f8d86c14138a322a562d014ab3d2f183
Łukasz Winowicz <lwinowicz@gmail.com>
2023-03-25 07:59:59 +0100

    [PATRO23-105] change appsettings (add object ApiSecuritySetting), add class ApiSecuritySettings

---
+++ src/api/app/Configuration/ApiSecuritySettings.cs
@@ -0,0 +1,13 @@
+namespace Intive.Patronage2023.Api.Configuration
+{
+	/// <summary>
+	/// text.
+	/// </summary>
+	public class ApiSecuritySettings
+	{
+		/// <summary>
+		/// text.
+		/// </summary>
+		public string[]? CorsAllowedOrigins { get; set; }
+	}
+}
