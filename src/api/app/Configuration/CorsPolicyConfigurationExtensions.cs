19c96a153498c241e202c19b4bb6d60c5d7c9cd6
Łukasz Winowicz <lwinowicz@gmail.com>
2023-03-27 18:55:41 +0200

    [PATRO23-105] replace Nested Conditional with Guard Clauses

--- src/api/app/Configuration/CorsPolicyConfigurationExtensions.cs
+++ src/api/app/Configuration/CorsPolicyConfigurationExtensions.cs
@@ -1,33 +1,36 @@
-namespace Intive.Patronage2023.Api.Configuration
+namespace Intive.Patronage2023.Api.Configuration;
+
+/// <summary>
+/// Static class CorsPolicyConfiguration.
+/// </summary>
+public static class CorsPolicyConfigurationExtensions
 {
 	/// <summary>
-	/// Static class CorsPolicyConfiguration.
+	/// Eetension method named that extends the IServiceCollection interface, that add Cross-Origin Resource Sharing (CORS) middleware to an application.
 	/// </summary>
-	public static class CorsPolicyConfigurationExtensions
-	{
-		/// <summary>
-		/// Eetension method named that extends the IServiceCollection interface, that add Cross-Origin Resource Sharing (CORS) middleware to an application.
-		/// </summary>
-		/// <param name="services">IServiceCollection object.</param>
-		/// <param name="configuration">IConfiguration object.</param>
-		/// <param name="corsPolicyName">Name of the CORS policy.</param>
-		public static void AddCors(
-			this IServiceCollection services, IConfiguration configuration, string corsPolicyName) => services.AddCors(options =>
+	/// <param name="services">IServiceCollection object.</param>
+	/// <param name="configuration">IConfiguration object.</param>
+	/// <param name="corsPolicyName">Name of the CORS policy.</param>
+	public static void AddCors(
+		this IServiceCollection services, IConfiguration configuration, string corsPolicyName) => services.AddCors(options =>
+		{
+			var apiSecuritySettings = configuration.GetSection("ApiSecuritySettings").Get<ApiSecuritySettings>();
+			if (apiSecuritySettings == null)
 			{
-				var apiSecuritySettings = configuration.GetSection("ApiSecuritySettings").Get<ApiSecuritySettings>();
-				if (apiSecuritySettings != null)
-				{
-					string[]? allowedOrigins = apiSecuritySettings.CorsAllowedOrigins;
-					if (allowedOrigins != null)
-					{
-						options.AddPolicy(corsPolicyName, builder =>
-						{
-							builder.AllowAnyMethod()
-							.AllowAnyHeader()
-							.WithOrigins(allowedOrigins);
-						});
-					}
-				}
+				return;
+			}
+
+			string[]? allowedOrigins = apiSecuritySettings?.CorsAllowedOrigins;
+			if (allowedOrigins is null || allowedOrigins.Length == 0)
+			{
+				return;
+			}
+
+			options.AddPolicy(corsPolicyName, builder =>
+			{
+				builder.AllowAnyMethod()
+				.AllowAnyHeader()
+				.WithOrigins(allowedOrigins);
 			});
-	}
+		});
 }
