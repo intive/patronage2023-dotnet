50fadbee6bcdbc1df9c5c1d6d1c286731599ed35
Piotr Paluszek <piotr.paluszek@nokia.com>
2023-03-31 10:07:58 +0200

    [PATRO23-37] Budget migration file formatting

--- src/modules/budget/infrastructure/Migrations/20230331075239_BudgetMigration.cs
+++ src/modules/budget/infrastructure/Migrations/20230331075239_BudgetMigration.cs
@@ -1,40 +1,38 @@
-﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Intive.Patronage2023.Modules.Budget.Infrastructure.Migrations
+namespace Intive.Patronage2023.Modules.Budget.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class BudgetMigration : Migration
 {
-    /// <inheritdoc />
-    public partial class BudgetMigration : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.EnsureSchema(
-                name: "Budgets");
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.EnsureSchema(
+			name: "Budgets");
 
-            migrationBuilder.CreateTable(
-                name: "Budget",
-                schema: "Budgets",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
-                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Budget", x => x.Id);
-                });
-        }
+		migrationBuilder.CreateTable(
+			name: "Budget",
+			schema: "Budgets",
+			columns: table => new
+			{
+				Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+				Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+				CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+			},
+			constraints: table =>
+			{
+				table.PrimaryKey("PK_Budget", x => x.Id);
+			});
+	}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Budget",
-                schema: "Budgets");
-        }
-    }
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropTable(
+			name: "Budget",
+			schema: "Budgets");
+	}
 }
