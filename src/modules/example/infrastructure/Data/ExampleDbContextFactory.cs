e9fa967dcf93382cfa15a7cde96497278111fed3
DawidBski <dawibski@gmail.com>
2023-04-01 12:55:39 +0200

    change connection string name

--- src/modules/example/infrastructure/Data/ExampleDbContextFactory.cs
+++ src/modules/example/infrastructure/Data/ExampleDbContextFactory.cs
@@ -2,26 +2,25 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
-namespace Intive.Patronage2023.Modules.Example.Infrastructure.Data
+namespace Intive.Patronage2023.Modules.Example.Infrastructure.Data;
+
+/// <summary>
+///  provides a new instance of a DbContext for use in design-time scenarios, such as running migrations.
+/// </summary>
+public class ExampleDbContextFactory : IDesignTimeDbContextFactory<ExampleDbContext>
 {
-	/// <summary>
-	///  provides a new instance of a DbContext for use in design-time scenarios, such as running migrations.
-	/// </summary>
-	public class ExampleDbContextFactory : IDesignTimeDbContextFactory<ExampleDbContext>
+	/// <inheritdoc/>
+	public ExampleDbContext CreateDbContext(string[] args)
 	{
-		/// <inheritdoc/>
-		public ExampleDbContext CreateDbContext(string[] args)
-		{
-			IConfigurationRoot configuration = new ConfigurationBuilder()
-				.SetBasePath(Directory.GetCurrentDirectory())
-				.AddJsonFile("appsettings.json")
-				.AddJsonFile("appsettings.Development.json")
-				.Build();
+		IConfigurationRoot configuration = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json")
+			.AddJsonFile("appsettings.Development.json")
+			.Build();
 
-			var optionsBuilder = new DbContextOptionsBuilder<ExampleDbContext>();
-			optionsBuilder.UseSqlServer(configuration.GetConnectionString("MigrationsDb"));
+		var optionsBuilder = new DbContextOptionsBuilder<ExampleDbContext>();
+		optionsBuilder.UseSqlServer(configuration.GetConnectionString("DockerDb"));
 
-			return new ExampleDbContext(optionsBuilder.Options);
-		}
+		return new ExampleDbContext(optionsBuilder.Options);
 	}
 }
