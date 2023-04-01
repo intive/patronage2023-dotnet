b520b61b24d9c283167afefc9199a51e91e68864
Łukasz Winowicz <lwinowicz@gmail.com>
2023-04-01 19:33:54 +0200

    [PATRO23-7] add working AuthenticationController (status 200OK and token)

---
+++ src/modules/example/api/Controllers/AuthenticationController.cs
@@ -0,0 +1,104 @@
+using System.Net;
+using System.Runtime.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Intive.Patronage2023.Modules.Example.Api.Controllers;
+
+/// <summary>
+/// text.
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+	/// <summary>
+	/// text.
+	/// </summary>
+	/// /<param name="username">Example identifier.</param>
+	/// <param name="password">Example name.</param>
+	/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+	[HttpPost("signin")]
+	public async Task<IActionResult> SignInUserAsync([FromForm] string username, [FromForm] string password)
+	{
+		using var client = new HttpClient();
+
+		var content = new FormUrlEncodedContent(new[]
+		{
+				new KeyValuePair<string, string>("username", username),
+				new KeyValuePair<string, string>("password", password),
+				new KeyValuePair<string, string>("client_id", "TestClient"),
+				new KeyValuePair<string, string>("client_secret", "Qe003KOmd5DOiJskv1I0kmD5Q6AigKv9"),
+				new KeyValuePair<string, string>("grant_type", "password"),
+		});
+
+		var response = await client.PostAsync("http://localhost:8080/realms/UserTest/protocol/openid-connect/token", content);
+
+		if (response.StatusCode == HttpStatusCode.Unauthorized)
+		{
+			return this.BadRequest();
+		}
+
+		string responseContent = await response.Content.ReadAsStringAsync();
+		var token = JsonConvert.DeserializeObject<Token>(responseContent);
+
+		string? accessToken = token?.AccessToken;
+
+		return this.Ok(accessToken);
+	}
+
+	/// <summary>
+	/// text.
+	/// </summary>
+	[DataContract]
+	public class Token
+	{
+		/// <summary>
+		/// AccessToken.
+		/// </summary>
+		[DataMember(Name = "access_token")]
+		public string? AccessToken { get; set; }
+
+		/// <summary>
+		/// ExpiresIn.
+		/// </summary>
+		[DataMember(Name = "expires_in")]
+		public int? ExpiresIn { get; set; }
+
+		/// <summary>
+		/// RefreshExpiresIn.
+		/// </summary>
+		[DataMember(Name = "refresh_expires_in")]
+		public int? RefreshExpiresIn { get; set; }
+
+		/// <summary>
+		/// RefreshToken.
+		/// </summary>
+		[DataMember(Name = "refresh_token")]
+		public string? RefreshToken { get; set; }
+
+		/// <summary>
+		/// TokenType.
+		/// </summary>
+		[DataMember(Name = "token_type")]
+		public string? TokenType { get; set; }
+
+		/// <summary>
+		/// NotBeforePolicy.
+		/// </summary>
+		[DataMember(Name = "not-before-policy")]
+		public int? NotBeforePolicy { get; set; }
+
+		/// <summary>
+		/// SessionState.
+		/// </summary>
+		[DataMember(Name = "session_state")]
+		public string? SessionState { get; set; }
+
+		/// <summary>
+		/// Scope.
+		/// </summary>
+		[DataMember(Name = "scope")]
+		public string? Scope { get; set; }
+	}
+}
