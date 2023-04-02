f3d6b09ce05d8542577fa053416122c81a7d3d09
Łukasz Winowicz <lwinowicz@gmail.com>
2023-04-02 19:09:24 +0200

    [PATRO23-7] change code 400 to 401, code work - need only refactor

--- src/modules/example/api/Controllers/AuthenticationController.cs
+++ src/modules/example/api/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Runtime.Serialization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -19,7 +20,7 @@ public class AuthenticationController : ControllerBase
 	/// <param name="password">User password.</param>
 	/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
 	/// <response code="200">Successfully signed in.</response>
-	/// <response code="400">Username or password is not valid.</response>
+	/// <response code="401">Username or password is not valid.</response>
 	/// <response code="500">Internal server error.</response>
 	[HttpPost("SignIn")]
 	public async Task<IActionResult> SignInUserAsync([FromForm] string username, [FromForm] string password)
@@ -48,6 +49,21 @@ public class AuthenticationController : ControllerBase
 		string? accessToken = token?.AccessToken;
 
 		return this.Ok(accessToken);
+	}
+
+	/// <summary>
+	/// Returns ok if authorized.
+	/// </summary>
+	/// <returns>Paged list of examples.</returns>
+	/// <response code="200">Returns 200Ok if authorized.</response>
+	/// <response code="401">If the user is unauthorized.</response>
+	[HttpGet]
+	[Authorize]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+	public IActionResult ReturnOkIfAuthorized()
+	{
+		return this.Ok();
 	}
 
 	/// <summary>
