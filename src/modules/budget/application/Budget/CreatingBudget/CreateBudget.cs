ff56c144b9dabe7642f09f18716cc4eeb9d9f31a
Aleksandra Lipert <ola.lipert@gmail.com>
2023-04-05 12:53:26 +0200

    [PATRO23-137] Add create budget command handler.

--- src/modules/budget/application/Budget/CreatingBudget/CreateBudget.cs
+++ src/modules/budget/application/Budget/CreatingBudget/CreateBudget.cs
@@ -1,14 +1,20 @@
-namespace Intive.Patronage2023.Modules.Budget.Application.Budget.CreatingBudget;
-
 using Intive.Patronage2023.Modules.Budget.Domain;
 using Intive.Patronage2023.Shared.Abstractions.Commands;
 
+namespace Intive.Patronage2023.Modules.Budget.Application.Budget.CreatingBudget;
 /// <summary>
 /// Create Budget command.
 /// </summary>
 /// <param name="Id">Budget identifier.</param>
 /// <param name="Name">Budget name.</param>
-public record CreateBudget(Guid Id, string Name);
+/// <param name="StartDate">Budget start date.</param>
+/// <param name="EndDate">Budget end date.</param>
+/// <param name="Limit">Budget limit.</param>
+/// <param name="Currency">Budget currency.</param>
+/// <param name="Description">Description.</param>
+/// <param name="IconName">IconName.</param>
+
+public record CreateBudget(Guid Id, string Name, DateTime StartDate, DateTime EndDate, Currency Currency, decimal Limit, string Description, string IconName);
 
 /// <summary>
 /// Create Budget.
@@ -29,9 +35,8 @@ public class HandleCreateBudget : ICommandHandler<CreateBudget>
 	/// <inheritdoc/>
 	public Task Handle(CreateBudget command)
 	{
-		var budget = BudgetAggregate.Create(command.Id, command.Name);
-
-		this.budgetRepository.Persist(budget);
+		// var budget = BudgetAggregate.Create(command.Id, command.Name, command.StartDate, command.EndDate, command.Currency, command.Limit);
+		// this.budgetRepository.Persist(budget);
 		return Task.CompletedTask;
 	}
 }
