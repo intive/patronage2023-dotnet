7e4338fc259915c5b0b288539fd09a78d4ce204e
Aleksandra Lipert <ola.lipert@gmail.com>
2023-04-05 13:01:17 +0200

    [PATRO23-137] Add budget initial migration.

---
+++ src/modules/budget/infrastructure/Migrations/20230331070307_InitialMigrationBudget.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intive.Patronage2023.Modules.Budget.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class InitialMigrationBudget : Migration
+{
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.EnsureSchema(
+			name: "Budgets");
+
+		migrationBuilder.CreateTable(
+			name: "Budget",
+			schema: "Budgets",
+			columns: table => new
+			{
+				Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+				Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+				CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+			},
+			constraints: table =>
+			{
+				table.PrimaryKey("PK_Budget", x => x.Id);
+			});
+	}
+
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropTable(
+			name: "Budget",
+			schema: "Budgets");
+	}
+}
