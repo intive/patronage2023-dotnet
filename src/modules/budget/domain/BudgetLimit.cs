5f825eee7e92d6d346c9e87785dd6017d56fb0e2
alanpawski <A.Pawski.AP@gmail.com>
2023-04-06 20:00:56 +0200

    Add budget aggregate with create method, and class for budget limit and budget period value objects

---
+++ src/modules/budget/domain/BudgetLimit.cs
@@ -0,0 +1,8 @@
+﻿namespace Intive.Patronage2023.Modules.Budget.Domain;
+
+/// <summary>
+/// Budget limit value object.
+/// </summary>
+/// <param name="Value">Value.</param>
+/// <param name="Currency">Currency.</param>
+public record class BudgetLimit(float Value, string Currency);
---
+++ src/modules/budget/domain/BudgetPeriod.cs
@@ -0,0 +1,8 @@
+﻿namespace Intive.Patronage2023.Modules.Budget.Domain;
+
+/// <summary>
+/// Budget period value object.
+/// </summary>
+/// <param name="StartDate">Start date.</param>
+/// <param name="EndDate">End date.</param>
+public record class BudgetPeriod(DateOnly StartDate, DateOnly EndDate);
