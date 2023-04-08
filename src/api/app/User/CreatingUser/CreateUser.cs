4dece53540d06b5e808b45393411bdd336e54c2c
Michał Szczepaniak <miher13@wp.pl>
2023-04-08 18:40:53 +0200

    [PATRO23-192] Add more detailed information to create user record

--- src/api/app/User/CreatingUser/CreateUser.cs
+++ src/api/app/User/CreatingUser/CreateUser.cs
@@ -6,8 +6,8 @@ namespace Intive.Patronage2023.Modules.Example.Api.User.CreatingUser;
 /// Create user command.
 /// </summary>
 /// <param name="Id">User identifier.</param>
-/// <param name="FirstName">First name.</param>
-/// <param name="LastName">Last name.</param>
-/// <param name="Password">Password.</param>
+/// <param name="FirstName">First name which user provides.</param>
+/// <param name="LastName">Last name which user provides.</param>
+/// <param name="Password">Password which user provides.</param>
 /// <param name="Email">Email which user provides.</param>
 public record CreateUser(Guid Id, string FirstName, string LastName, string Password, string Email) : ICommand;
