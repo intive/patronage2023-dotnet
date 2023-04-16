136f2cbc62496a91a3e29615065fdf33d68fba1e
KlaudiaGajocha <klaudiagajocha99@gmail.com>
2023-04-16 17:16:51 +0200

    [PATRO23-123] deleting mock usage and fixing the fileName notation

--- tests/integration/QueryResponseTests.cs
+++ tests/integration/QueryResponseTests.cs
@@ -1,98 +1,46 @@
 using Intive.Patronage2023.Modules.Example.Domain;
-using Moq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace QueryResponseTests
+namespace Intive.Patronage2023.Modules.Example.Tests
 {
-    public interface IQueryHandler<in T, TResult>
-    {
-        /// <summary>
-        /// Handles the query.
-        /// </summary>
-        /// <param name="query">Query to handle.</param>
-        /// <returns>Task that represents asynchronous operation which returns query resposne.</returns>
-        Task<TResult> Handle(T query);
-    }
+	public class GetExamplesTests
+	{
+		private readonly ExampleRepository exampleRepository;
+		private readonly GetExamplesQueryHandler handler;
 
-    public class ExamplesQuery
-    {
-        public bool IncludeArchived { get; set; }
-    }
+		public GetExamplesTests()
+		{
+			var exampleDbContext = new ExampleDbContext();
+			exampleRepository = new ExampleRepository(exampleDbContext);
+			handler = new GetExamplesQueryHandler(exampleRepository);
+		}
 
-    public class Example
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public bool IsArchived { get; set; }
-    }
+		[Fact]
+		public async Task Handle_ValidQuery_ReturnsExamples()
+		{
+			// Arrange
+			var query = new ExamplesQuery();
 
-    public interface IExampleRepository
-    {
-        Task<List<Example>> GetExamplesAsync(bool includeArchived = false);
-    }
+			// Act
+			var result = await handler.Handle(query);
 
-    public class GetExamplesQueryHandler : IQueryHandler<ExamplesQuery, List<Example>>
-    {
-        private readonly IExampleRepository _exampleRepository;
+			// Assert
+			Assert.NotNull(result);
+		}
 
-        public GetExamplesQueryHandler(IExampleRepository exampleRepository)
-        {
-            _exampleRepository = exampleRepository;
-        }
+		[Fact]
+		public async Task Handle_EmptyQuery_ReturnsAllExamples()
+		{
+			// Arrange
+			var query = new ExamplesQuery { IncludeArchived = true };
 
-        public async Task<List<Example>> Handle(ExamplesQuery query)
-        {
-            return await _exampleRepository.GetExamplesAsync(query.IncludeArchived);
-        }
-    }
+			// Act
+			var result = await handler.Handle(query);
 
-    public class GetExamplesTests
-    {
-        private readonly Mock<IExampleRepository> _exampleRepositoryMock;
-        private readonly GetExamplesQueryHandler _handler;
-
-        public GetExamplesTests()
-        {
-            _exampleRepositoryMock = new Mock<IExampleRepository>();
-            _handler = new GetExamplesQueryHandler(_exampleRepositoryMock.Object);
-        }
-
-        [Fact]
-        public async Task Handle_ValidQuery_ReturnsExamples()
-        {
-            // Arrange
-            var query = new ExamplesQuery();
-            var examples = new List<Example> { new Example { Id = 1, Name = "Example 1" } };
-            _exampleRepositoryMock.Setup(repo => repo.GetExamplesAsync()).ReturnsAsync(examples);
-
-            // Act
-            var result = await _handler.Handle(query);
-
-            // Assert
-            Assert.Equal(examples, result);
-        }
-
-        [Fact]
-        public async Task Handle_EmptyQuery_ReturnsAllExamples()
-        {
-            // Arrange
-            var query = new ExamplesQuery { IncludeArchived = true };
-            var examples = new List<Example>
-        {
-            new Example { Id = 1, Name = "Example 1" },
-            new Example { Id = 2, Name = "Example 2", IsArchived = true },
-            new Example { Id = 3, Name = "Example 3" }
-        };
-            _exampleRepositoryMock.Setup(repo => repo.GetExamplesAsync(query.IncludeArchived)).ReturnsAsync(examples);
-
-            // Act
-            var result = await _handler.Handle(query);
-
-            // Assert
-            Assert.Equal(examples, result);
-        }
-    }
-
+			// Assert
+			Assert.NotNull(result);
+		}
+	}
 }
