4e57f3b1b069251c5ffe66b31e48c6dc54780df0
Aleksandra Lipert <ola.lipert@gmail.com>
2023-04-19 19:34:10 +0200

    [PATRO23-33] Fix formatting.

--- src/modules/budget/application/Budget/GettingBudgets/BudgetInfo.cs
+++ src/modules/budget/application/Budget/GettingBudgets/BudgetInfo.cs
@@ -5,18 +5,18 @@ namespace Intive.Patronage2023.Modules.Budget.Application.Budget.GettingBudgets;
 /// </summary>
 public record BudgetInfo()
 {
-    /// <summary>
-    /// Budget it.
-    /// </summary>
-    public Guid Id { get; init; }
+	/// <summary>
+	/// Budget id.
+	/// </summary>
+	public Guid Id { get; init; }
 
-    /// <summary>
-    /// Budget name.
-    /// </summary>
-    public string Name { get; init; } = null!;
+	/// <summary>
+	/// Budget name.
+	/// </summary>
+	public string Name { get; init; } = null!;
 
-    /// <summary>
-    /// Budget creation date.
-    /// </summary>
-    public DateTime CreatedOn { get; init; }
+	/// <summary>
+	/// Budget creation date.
+	/// </summary>
+	public DateTime CreatedOn { get; init; }
 }
