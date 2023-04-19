c756630afa41c04c9231c4fdf0d79e914c934353
Aleksandra Lipert <ola.lipert@gmail.com>
2023-04-19 13:19:48 +0200

    [PATRO23-226] Add validation rules without the one for name/userid.

--- src/modules/budget/application/Budget/CreatingBudget/CreateBudgetValidator.cs
+++ src/modules/budget/application/Budget/CreatingBudget/CreateBudgetValidator.cs
@@ -17,9 +17,44 @@ public class CreateBudgetValidator : AbstractValidator<CreateBudget>
 	public CreateBudgetValidator(IBudgetRepository budgetRepository)
 	{
 		this.budgetRepository = budgetRepository;
-		this.RuleFor(budget => budget.Id).NotEmpty().NotNull();
-		this.RuleFor(budget => budget.Name).NotEmpty().NotNull().WithMessage("{PropertyName} must not be empty");
-		this.RuleFor(budget => new { budget.Period.StartDate, budget.Period.EndDate }).Must(x => x.StartDate < x.EndDate).WithMessage("The start date must be earlier than the end date");
-		this.RuleFor(budget => budget.Name).Must(x => !this.budgetRepository.ExistsByName(x)).WithMessage("{PropertyName} already exists. Choose a different name");
+		this.RuleFor(budget => budget.Id)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("{PropertyName} must not be empty");
+		this.RuleFor(budget => budget.Name)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("{PropertyName} must not be empty");
+		this.RuleFor(budget => budget.Name)
+			.Length(3, 30)
+			.WithMessage("{PropertyName} must contain between 3 and 30 characters");
+		this.RuleFor(budget => budget.Period)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("{PropertyName} must not be empty");
+		this.RuleFor(budget => new { budget.Period.StartDate, budget.Period.EndDate })
+			.Must(x => x.StartDate <= x.EndDate)
+			.WithMessage("The start date must be earlier than the end date");
+
+		// this.RuleFor(budget => budget.Name)
+		// .Must(x => !this.budgetRepository.ExistsByName(x))
+		// .WithMessage("{PropertyName} already exists. Choose a different name");
+		this.RuleFor(budget => budget.Limit)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("{PropertyName} must not be empty");
+		this.RuleFor(budget => budget.Limit.Value)
+			.GreaterThan(0)
+			.WithMessage("{PropertyName} must be greater than 0");
+		this.RuleFor(budget => budget.Limit.Currency)
+			.IsInEnum()
+			.NotEmpty()
+			.WithMessage("{PropertyName} must not be empty");
+		this.RuleFor(budget => budget.Description)
+			.MaximumLength(50)
+			.WithMessage("{PropertyName} is too long.");
+		this.RuleFor(budget => budget.IconName.Length)
+			.LessThan(256)
+			.WithMessage("{PropertyName} is too long.");
 	}
 }
--- src/modules/budget/domain/IBudgetRepository.cs
+++ src/modules/budget/domain/IBudgetRepository.cs
@@ -10,7 +10,8 @@ public interface IBudgetRepository : IRepository<BudgetAggregate, Guid>
 	/// <summary>
 	/// Checks if budget of given name exists.
 	/// </summary>
+	/// <param name="userId">Budget owner id.</param>
 	/// <param name="name">Budget name.</param>
 	/// <returns>True if exists.</returns>
-	bool ExistsByName(string name);
+	bool ExistsByName(Guid userId, string name);
 }
