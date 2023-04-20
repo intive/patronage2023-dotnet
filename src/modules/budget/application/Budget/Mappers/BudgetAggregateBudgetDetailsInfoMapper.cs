9ead1c4d3f4737e0f68409645c3917a4d61e98ad
pzajaczkowski <demoon.pps@gmail.com>
2023-04-21 01:29:45 +0200

    [PATRO23-227] Create structure for GetBudgetDetails

---
+++ src/modules/budget/application/Budget/Mappers/BudgetAggregateBudgetDetailsInfoMapper.cs
@@ -0,0 +1,18 @@
+using Intive.Patronage2023.Modules.Budget.Application.Budget.GettingBudgetDetails;
+using Intive.Patronage2023.Modules.Budget.Domain;
+
+namespace Intive.Patronage2023.Modules.Budget.Application.Budget.Mappers;
+
+/// <summary>
+/// Mapper class.
+/// </summary>
+public static class BudgetAggregateBudgetDetailsInfoMapper
+{
+	/// <summary>
+	/// Mapping method.
+	/// </summary>
+	/// <param name="entity">Entity to be mapped.</param>
+	/// <returns>Returns <ref name="BudgetDetailsInfo"/>Budget details information.</returns>
+	public static BudgetDetailsInfo Map(BudgetAggregate entity) =>
+		new() { Id = entity.Id, Name = entity.Name, CreatedOn = entity.CreatedOn };
+}
