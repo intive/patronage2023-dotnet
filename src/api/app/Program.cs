d422774c64bed528a76d6e8d320aa10f9c2b2e75
Michał Szczepaniak <miher13@wp.pl>
2023-04-25 17:22:19 +0200

    [PATRO23-158] Remove unused method

--- src/api/app/Program.cs
+++ src/api/app/Program.cs
@@ -57,10 +57,10 @@ builder.Services.AddControllers(options =>
 			.RequireAuthenticatedUser()
 			.Build())));
 
-builder.Services.AddFromAssemblies(typeof(IDomainEventHandler<>));
-builder.Services.AddFromAssemblies(typeof(IEventDispatcher<>));
-builder.Services.AddFromAssemblies(typeof(ICommandHandler<>));
-builder.Services.AddFromAssemblies(typeof(IQueryHandler<,>));
+builder.Services.AddFromAssemblies(typeof(IDomainEventHandler<>), typeof(IDomainEventHandler<>).Assembly);
+builder.Services.AddFromAssemblies(typeof(IEventDispatcher<>), typeof(IEventDispatcher<>).Assembly);
+builder.Services.AddFromAssemblies(typeof(ICommandHandler<>), typeof(ICommandHandler<>).Assembly);
+builder.Services.AddFromAssemblies(typeof(IQueryHandler<,>), typeof(IQueryHandler<,>).Assembly);
 
 builder.Services.AddScoped<ICommandBus, CommandBus>();
 builder.Services.AddScoped<IQueryBus, QueryBus>();
