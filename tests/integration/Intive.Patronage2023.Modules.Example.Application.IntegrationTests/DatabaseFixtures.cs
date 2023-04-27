ea11f0d2af406556c58b249cdb88dd3c0624397b
Mateusz Giedzinski <mateusz.giedzinski@intive.com>
2023-04-28 00:52:23 +0200

    PATRO23-123-Change used port.

--- tests/integration/Intive.Patronage2023.Modules.Example.Application.IntegrationTests/DatabaseFixtures.cs
+++ tests/integration/Intive.Patronage2023.Modules.Example.Application.IntegrationTests/DatabaseFixtures.cs
@@ -1,6 +1,9 @@
 using System.Data.Common;
+
 using DotNet.Testcontainers.Builders;
+
 using FluentAssertions;
+
 using Intive.Patronage2023.Modules.Budget.Application.Budget.GettingBudgets;
 using Intive.Patronage2023.Modules.Budget.Domain;
 using Intive.Patronage2023.Modules.Budget.Infrastructure.Data;
@@ -15,97 +18,99 @@ using Microsoft.Extensions.DependencyInjection;
 using IContainer = DotNet.Testcontainers.Containers.IContainer;
 
 namespace Intive.Patronage2023.Modules.Example.Application.IntegrationTests;
+
 public class MsSqlTests : IAsyncLifetime
 {
-    public const string Database = "patronage2023";
-    public const string Username = "sa";
-    public const string Password = "yourStrong(!)Password";
-    public const ushort MsSqlPort = 1433;
+	public const string Database = "patronage2023";
+	public const string Username = "sa";
+	public const string Password = "yourStrong(!)Password";
+	public const ushort MsSqlPort = 1433;
+	public const ushort MappedPort = 5000;
 
-    public readonly IContainer _mssqlContainer = new ContainerBuilder()
-        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-        .WithPortBinding("1433", MsSqlPort.ToString())
-        .WithEnvironment("ACCEPT_EULA", "Y")
-        .WithEnvironment("MSSQL_SA_PASSWORD", Password)
-        .WithWaitStrategy(Wait.ForUnixContainer().UntilCommandIsCompleted("/opt/mssql-tools/bin/sqlcmd", "-Q", "SELECT 1;"))
-        .Build();
+	public readonly IContainer _mssqlContainer = new ContainerBuilder()
+		.WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+		.WithPortBinding(MappedPort.ToString(), MsSqlPort.ToString())
+		.WithEnvironment("ACCEPT_EULA", "Y")
+		.WithEnvironment("MSSQL_SA_PASSWORD", Password)
+		.WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5000).UntilCommandIsCompleted("/opt/mssql-tools/bin/sqlcmd", "-Q", "SELECT 1;"))
+		.Build();
 
-    public Task InitializeAsync()
-    {
-        return this._mssqlContainer.StartAsync();
-    }
+	public Task InitializeAsync()
+	{
+		return this._mssqlContainer.StartAsync();
+	}
 
-    public Task DisposeAsync()
-    {
-        return this._mssqlContainer.DisposeAsync().AsTask();
-    }
+	public Task DisposeAsync()
+	{
+		return this._mssqlContainer.DisposeAsync().AsTask();
+	}
 }
 
 public class ExampleTests : IClassFixture<MsSqlTests>, IDisposable
 {
-    private readonly WebApplicationFactory<Intive.Patronage2023.Api.Program> _webApplicationFactory;
+	private readonly WebApplicationFactory<Intive.Patronage2023.Api.Program> _webApplicationFactory;
 
-    public ExampleTests(MsSqlTests fixture)
-    {
+	public ExampleTests(MsSqlTests fixture)
+	{
 		var clientOptions = new WebApplicationFactoryClientOptions
 		{
 			AllowAutoRedirect = false
 		};
 		this._webApplicationFactory = new CustomWebApplicationFactory(fixture);
-    }
+	}
 
-    [Fact]
-    public async Task Handle_WhenCalled_ShouldReturnPagedList()
-    {
-        // Arrange
-        var scope = this._webApplicationFactory.Services.CreateScope();
-        var dbContext = scope.ServiceProvider.GetService<BudgetDbContext>();
-        var command = BudgetAggregate.Create(
-	        Guid.NewGuid(),
-	        "example name",
-	        Guid.NewGuid(),
-	        new Money(1, (Currency)1),
-	        new Period(DateTime.Now, DateTime.Now.AddDays(1)),
-	        "icon",
-	        "description");
-        
-        dbContext!.Add(command);
-        await dbContext.SaveChangesAsync();
-        var query = new GetBudgets();
-        var handler = new GetBudgetsQueryHandler(dbContext);
+	[Fact]
+	public async Task Handle_WhenCalled_ShouldReturnPagedList()
+	{
+		// Arrange
+		var scope = this._webApplicationFactory.Services.CreateScope();
+		var dbContext = scope.ServiceProvider.GetService<BudgetDbContext>();
+		var command = BudgetAggregate.Create(
+			Guid.NewGuid(),
+			"example name",
+			Guid.NewGuid(),
+			new Money(1, (Currency)1),
+			new Period(DateTime.Now, DateTime.Now.AddDays(1)),
+			"icon",
+			"description");
 
-        // Act
-        var result = await handler.Handle(query, CancellationToken.None);
+		dbContext!.Add(command);
+		await dbContext.SaveChangesAsync();
+		var query = new GetBudgets();
+		var handler = new GetBudgetsQueryHandler(dbContext);
 
-        // Assert
-        result.Should().NotBeNull();
-        result.Items.Should().HaveCount(1);
-    }
+		// Act
+		var result = await handler.Handle(query, CancellationToken.None);
 
-    public void Dispose()
-    {
-        this._webApplicationFactory.Dispose();
-    }
+		// Assert
+		result.Should().NotBeNull();
+		result.Items.Should().HaveCount(1);
+	}
 
-    private class CustomWebApplicationFactory : WebApplicationFactory<Intive.Patronage2023.Api.Program>
-    {
-        private readonly string connectionString;
+	public void Dispose()
+	{
+		this._webApplicationFactory.Dispose();
+	}
 
-        public CustomWebApplicationFactory(MsSqlTests fixture)
-        {
-            this.connectionString =
-                $"Server=localhost,1433;Database={MsSqlTests.Database};User Id={MsSqlTests.Username};Password={MsSqlTests.Password};TrustServerCertificate=True";
-        }
+	private class CustomWebApplicationFactory : WebApplicationFactory<Intive.Patronage2023.Api.Program>
+	{
+		private readonly string connectionString;
 
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices(
-                services =>
-                {
-                services.Remove(services.SingleOrDefault(service => typeof(DbContextOptions<BudgetDbContext>) == service.ServiceType)!);
-                services.Remove(services.SingleOrDefault(service => typeof(DbConnection) == service.ServiceType)!);
-                    services.AddDbContext<BudgetDbContext>((_, option) => option.UseSqlServer(this.connectionString));
-                });
-        }
-    }
+		public CustomWebApplicationFactory(MsSqlTests fixture)
+		{
+			this.connectionString =
+				$"Server=localhost,{MsSqlTests.MappedPort};Database={MsSqlTests.Database};User Id={MsSqlTests.Username};Password={MsSqlTests.Password};TrustServerCertificate=True";
+		}
+
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			builder.ConfigureServices(
+				services =>
+				{
+					services.Remove(services.SingleOrDefault(service => typeof(DbContextOptions<BudgetDbContext>) == service.ServiceType)!);
+					services.Remove(services.SingleOrDefault(service => typeof(DbConnection) == service.ServiceType)!);
+					services.AddDbContext<BudgetDbContext>((_, option) => option.UseSqlServer(this.connectionString));
+				});
+		}
+	}
 }
