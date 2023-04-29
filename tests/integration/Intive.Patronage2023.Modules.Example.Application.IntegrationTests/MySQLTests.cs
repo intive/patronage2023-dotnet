5c66a424fef44fb387db6a4154decacf1e689abe
KlaudiaGajocha <klaudiagajocha99@gmail.com>
2023-04-29 09:09:35 +0200

    [PATRO23-123] Change namespace

--- tests/integration/Intive.Patronage2023.Modules.Example.Application.IntegrationTests/MySQLTests.cs
+++ tests/integration/Intive.Patronage2023.Modules.Example.Application.IntegrationTests/MySQLTests.cs
@@ -13,34 +13,33 @@ using Microsoft.Extensions.DependencyInjection;
 using IContainer = DotNet.Testcontainers.Containers.IContainer;
 using System.Threading.Tasks;
 
-namespace Intive.Patronage2023.Modules.Example.Application.IntegrationTests
+namespace Intive.Patronage2023.Modules.Example.Application.IntegrationTests;
+
+public class MsSqlTests : IAsyncLifetime
 {
-	public class MsSqlTests : IAsyncLifetime
+	public const string Database = "patronage2023";
+	public const string Username = "sa";
+	public const string Password = "yourStrong(!)Password";
+	public const ushort MsSqlPort = 1433;
+	public const ushort MappedPort = 5000;
+
+	public readonly IContainer _mssqlContainer = new ContainerBuilder()
+		.WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+		.WithPortBinding(MappedPort.ToString(), MsSqlPort.ToString())
+		.WithEnvironment("ACCEPT_EULA", "Y")
+		.WithEnvironment("SQLCMDUSER", Username)
+		.WithEnvironment("SQLCMDPASSWORD", Password)
+		.WithEnvironment("MSSQL_SA_PASSWORD", Password)
+		.WithWaitStrategy(Wait.ForUnixContainer().UntilCommandIsCompleted("/opt/mssql-tools/bin/sqlcmd", "-Q", "SELECT 1;"))
+		.Build();
+
+	public Task InitializeAsync()
 	{
-		public const string Database = "patronage2023";
-		public const string Username = "sa";
-		public const string Password = "yourStrong(!)Password";
-		public const ushort MsSqlPort = 1433;
-		public const ushort MappedPort = 5000;
+		return this._mssqlContainer.StartAsync();
+	}
 
-		public readonly IContainer _mssqlContainer = new ContainerBuilder()
-			.WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-			.WithPortBinding(MappedPort.ToString(), MsSqlPort.ToString())
-			.WithEnvironment("ACCEPT_EULA", "Y")
-			.WithEnvironment("SQLCMDUSER", Username)
-			.WithEnvironment("SQLCMDPASSWORD", Password)
-			.WithEnvironment("MSSQL_SA_PASSWORD", Password)
-			.WithWaitStrategy(Wait.ForUnixContainer().UntilCommandIsCompleted("/opt/mssql-tools/bin/sqlcmd", "-Q", "SELECT 1;"))
-			.Build();
-
-		public Task InitializeAsync()
-		{
-			return this._mssqlContainer.StartAsync();
-		}
-
-		public Task DisposeAsync()
-		{
-			return this._mssqlContainer.DisposeAsync().AsTask();
-		}
+	public Task DisposeAsync()
+	{
+		return this._mssqlContainer.DisposeAsync().AsTask();
 	}
 }
