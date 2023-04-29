5e499f466bc878636537eb815efa9ed145d4ae60
DawidBski <dawibski@gmail.com>
2023-04-29 22:09:39 +0200

    [PATRO23-231-229] add tests

---
+++ tests/unit/Intive.Patronage2023.Budget.Application.Tests/CreateBudgetTransactionCommandHandlerTests.cs
@@ -0,0 +1,67 @@
+using Bogus;
+using Intive.Patronage2023.Modules.Budget.Application.Budget.CreatingBudgetTransaction;
+using Intive.Patronage2023.Modules.Budget.Contracts.TransactionEnums;
+using Intive.Patronage2023.Modules.Budget.Contracts.ValueObjects;
+using Intive.Patronage2023.Modules.Budget.Domain;
+
+using Moq;
+
+using Xunit;
+
+namespace Intive.Patronage2023.Budget.Application.Tests;
+
+/// <summary>
+/// Test that checks the behavior of a query handler class "CreateBudgetTransaction".
+/// </summary>
+public class CreateBudgetTransactionCommandHandlerTests
+{
+	private readonly Mock<IBudgetTransactionRepository> budgetTransactionRepositoryMock;
+	private readonly CreateBudgetTransactionCommandHandler handleCreateBudgetTransaction;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CreateBudgetTransactionCommandHandlerTests"/> class.
+	/// </summary>
+	public CreateBudgetTransactionCommandHandlerTests()
+	{
+		this.budgetTransactionRepositoryMock = new Mock<IBudgetTransactionRepository>();
+		this.handleCreateBudgetTransaction = new CreateBudgetTransactionCommandHandler(this.budgetTransactionRepositoryMock.Object);
+	}
+
+	/// <summary>
+	/// Test that check if the method correctly creates an instance of "BudgetTransactionAggregate" with the expected values
+	/// and then passes that instance to a mocked implementation of "IBudgetTransactionRepository" to persist it.
+	/// The test uses a "Mock" object to simulate the behavior of the "IBudgetTransactionRepository"
+	/// interface and verify that the "Persist" method is called once with the expected values.
+	/// </summary>
+	[Fact]
+	public async void Handle_WhenCalled_ShouldCreateBudgetTransactionWithCorrectValues()
+	{
+		// Arrange
+		var cancellationToken = CancellationToken.None;
+		var id = new TransactionId(new Faker().Random.Guid());
+		var budgetId = new BudgetId(new Faker().Random.Guid());
+		var type = new Faker().Random.Enum<TransactionType>();
+		string name = new Faker().Name.FirstName();
+		decimal value = new Faker().Random.Decimal(19, 4);
+		var category = new Faker().Random.Enum<CategoryType>();
+		var createdDate = new Faker().Date.Recent();
+		if (type == TransactionType.Expense)
+			value *= -1;
+
+		// Act
+		await this.handleCreateBudgetTransaction.Handle(new CreateBudgetTransaction(type, id.Value, budgetId.Value, name, value, category, createdDate), cancellationToken);
+
+		// Assert
+		this.budgetTransactionRepositoryMock.Verify(
+			r =>
+				r.Persist(It.Is<BudgetTransactionAggregate>(e =>
+				e.Id == id &&
+				e.BudgetId == budgetId &&
+				e.TransactionType == type &&
+				e.Name == name &&
+				e.Value == value &&
+				e.CategoryType == category &&
+				e.BudgetTransactionDate == createdDate)),
+			Times.Once);
+	}
+}
---
+++ tests/unit/Intive.Patronage2023.Budget.Domain.Tests/BudgetTransactionAggregateTests.cs
@@ -0,0 +1,60 @@
+using Bogus;
+using FluentAssertions;
+
+using Intive.Patronage2023.Modules.Budget.Contracts.TransactionEnums;
+using Intive.Patronage2023.Modules.Budget.Contracts.ValueObjects;
+using Intive.Patronage2023.Modules.Budget.Domain;
+
+using Xunit;
+
+namespace Intive.Patronage2023.Budget.Domain.Tests;
+
+/// <summary>
+/// Test that checks the behavior of a aggregate class "BudgetTransactionAggregate".
+/// </summary>
+public class BudgetTransactionAggregateTests
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BudgetTransactionAggregateTests"/> class.
+	/// </summary>
+	public BudgetTransactionAggregateTests()
+	{
+	}
+
+	/// <summary>
+	/// Budget Transaction Aggregate test with proper data.
+	/// </summary>
+	[Fact]
+	public void Create_WhenPassedProperData_ShouldCreateBudgetTransactionAggregate()
+	{
+		// Arrange
+		var id = new TransactionId(new Faker().Random.Guid());
+		var budgetId = new BudgetId(new Faker().Random.Guid());
+		var type = new Faker().Random.Enum<TransactionType>();
+		string name = new Faker().Name.FirstName();
+		decimal value = new Faker().Random.Decimal(19, 4);
+		var category = new Faker().Random.Enum<CategoryType>();
+		var createdDate = new Faker().Date.Recent();
+		if (type == TransactionType.Expense)
+			value *= -1;
+
+		// Act
+		var budgetTransactionAggregate = BudgetTransactionAggregate.Create(id, budgetId, type, name, value, category, createdDate);
+
+		// Assert
+		budgetTransactionAggregate.Should()
+			.NotBeNull()
+			.And
+			.BeEquivalentTo(
+				new
+				{
+					Id = id,
+					BudgetId = budgetId,
+					TransactionType = type,
+					Name = name,
+					Value = value,
+					CategoryType = category,
+					BudgetTransactionDate = createdDate
+				});
+	}
+}
