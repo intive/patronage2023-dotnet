dbdb2c539f9e75be697b07d63ceebc6959c730d1
Michał Szczepaniak <miher13@wp.pl>
2023-05-11 11:50:53 +0200

    [PATRO23-299] fix sample data.

--- src/modules/budget/api/Controllers/BudgetController.cs
+++ src/modules/budget/api/Controllers/BudgetController.cs
@@ -415,7 +415,7 @@ public class BudgetController : ControllerBase
 	/// <param name="startDate">Start Date in which we want to get statistics.</param>
 	/// <param name="endDate">End date in which we want to get statistics.</param>
 	/// <remarks>
-	/// Sample Date Points:
+	/// Sample Id and Date Points:
 	///
 	///         "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
 	///         "startDate": "2023-04-20T19:14:20.152Z",
