6430595bb5f804d074be4f08200713b5538e809c
DawidBski <dawibski@gmail.com>
2023-05-13 13:46:57 +0200

    [PATRO23-314-323] add rules and messages

--- src/modules/budget/application/Budget/CancelBudgetTransaction/CancelBudgetTransactionValidator.cs
+++ src/modules/budget/application/Budget/CancelBudgetTransaction/CancelBudgetTransactionValidator.cs
@@ -11,14 +11,17 @@ namespace Intive.Patronage2023.Modules.Budget.Application.Budget.CancelBudgetTra
 public class CancelBudgetTransactionValidator : AbstractValidator<CancelBudgetTransaction>
 {
 	private readonly IRepository<BudgetTransactionAggregate, TransactionId> budgetTransactionRepository;
+	private readonly IRepository<BudgetAggregate, BudgetId> budgetRepository;
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="CancelBudgetTransactionValidator"/> class.
 	/// </summary>
 	/// <param name="budgetTransactionRepository">Budget Transaction Repository.</param>
-	public CancelBudgetTransactionValidator(IRepository<BudgetTransactionAggregate, TransactionId> budgetTransactionRepository)
+	/// <param name="budgetRepository">Budget Repository.</param>
+	public CancelBudgetTransactionValidator(IRepository<BudgetTransactionAggregate, TransactionId> budgetTransactionRepository, IRepository<BudgetAggregate, BudgetId> budgetRepository)
 	{
 		this.budgetTransactionRepository = budgetTransactionRepository;
+		this.budgetRepository = budgetRepository;
 
 		this.RuleFor(transaction => transaction.BudgetId)
 			.NotEmpty()
@@ -28,8 +31,29 @@ public class CancelBudgetTransactionValidator : AbstractValidator<CancelBudgetTr
 			.NotEmpty()
 			.NotNull();
 
+		this.RuleFor(transaction => transaction.TransactionId)
+			.MustAsync(async (x, cancellation) => await this.IsTransactionExists(x))
+			.WithMessage("Transaction doesn't exist.");
+
+		this.RuleFor(transaction => transaction.BudgetId)
+			.MustAsync(async (x, cancellation) => await this.IsBudgetExists(x))
+			.WithMessage("Budget doesn't exist.");
+
 		this.RuleFor(transaction => transaction)
-			.MustAsync(async (x, cancellation) => await this.IsTransactionBelongingToBudget(x.BudgetId, x.TransactionId));
+			.MustAsync(async (x, cancellation) => await this.IsTransactionBelongingToBudget(x.BudgetId, x.TransactionId))
+			.WithMessage("This transaction does not belong to the specified budget.");
+	}
+
+	private async Task<bool> IsTransactionExists(Guid transactionId)
+	{
+		BudgetTransactionAggregate? transaction = await this.budgetTransactionRepository.GetById(new TransactionId(transactionId));
+		return transaction != null;
+	}
+
+	private async Task<bool> IsBudgetExists(Guid budgetId)
+	{
+		BudgetAggregate? budget = await this.budgetRepository.GetById(new BudgetId(budgetId));
+		return budget != null;
 	}
 
 	private async Task<bool> IsTransactionBelongingToBudget(Guid budgetId, Guid transactionId)
