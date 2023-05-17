f320d7f0c00e00b53d1893b8deddf82e2b2ebdc8
pzajaczkowski <demoon.pps@gmail.com>
2023-05-17 12:28:25 +0200

    [PATRO23-312] Fix GetBudgetTransactions.cs formatting

--- src/modules/budget/application/Budget/GettingBudgetTransactions/GetBudgetTransactions.cs
+++ src/modules/budget/application/Budget/GettingBudgetTransactions/GetBudgetTransactions.cs
@@ -15,25 +15,25 @@ namespace Intive.Patronage2023.Modules.Budget.Application.Budget.GettingBudgetTr
 /// </summary>
 public record GetBudgetTransactions : IQuery<PagedList<BudgetTransactionInfo>>, IPageableQuery
 {
-    /// <summary>
-    /// The amount of data to return.
-    /// </summary>
-    public int PageSize { get; set; }
+	/// <summary>
+	/// The amount of data to return.
+	/// </summary>
+	public int PageSize { get; set; }
 
-    /// <summary>
-    /// Requested page.
-    /// </summary>
-    public int PageIndex { get; set; }
+	/// <summary>
+	/// Requested page.
+	/// </summary>
+	public int PageIndex { get; set; }
 
-    /// <summary>
-    /// Transaction type to filter. Null for all.
-    /// </summary>
-    public TransactionType? TransactionType { get; set; }
+	/// <summary>
+	/// Transaction type to filter. Null for all.
+	/// </summary>
+	public TransactionType? TransactionType { get; set; }
 
-    /// <summary>
-    /// Budget Id.
-    /// </summary>
-    public BudgetId BudgetId { get; init; }
+	/// <summary>
+	/// Budget Id.
+	/// </summary>
+	public BudgetId BudgetId { get; init; }
 }
 
 /// <summary>
@@ -41,36 +41,36 @@ public record GetBudgetTransactions : IQuery<PagedList<BudgetTransactionInfo>>, 
 /// </summary>
 public class GetTransactionsQueryHandler : IQueryHandler<GetBudgetTransactions, PagedList<BudgetTransactionInfo>>
 {
-    private readonly BudgetDbContext budgetDbContext;
+	private readonly BudgetDbContext budgetDbContext;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="GetTransactionsQueryHandler"/> class.
-    /// </summary>
-    /// <param name="budgetDbContext">Budget dbContext.</param>
-    public GetTransactionsQueryHandler(BudgetDbContext budgetDbContext)
-    {
-        this.budgetDbContext = budgetDbContext;
-    }
+	/// <summary>
+	/// Initializes a new instance of the <see cref="GetTransactionsQueryHandler"/> class.
+	/// </summary>
+	/// <param name="budgetDbContext">Budget dbContext.</param>
+	public GetTransactionsQueryHandler(BudgetDbContext budgetDbContext)
+	{
+		this.budgetDbContext = budgetDbContext;
+	}
 
-    /// <summary>
-    /// GetBudgets query handler.
-    /// </summary>
-    /// <param name="query">Query.</param>
-    /// <param name="cancellationToken">cancellation token.</param>
-    /// <returns>Paged list of Budgets.</returns>
-    public async Task<PagedList<BudgetTransactionInfo>> Handle(GetBudgetTransactions query, CancellationToken cancellationToken)
-    {
-        var budgets = this.budgetDbContext.Transaction.AsQueryable()
-            .For(query.BudgetId)
-            .WithType(query.TransactionType);
+	/// <summary>
+	/// GetBudgets query handler.
+	/// </summary>
+	/// <param name="query">Query.</param>
+	/// <param name="cancellationToken">cancellation token.</param>
+	/// <returns>Paged list of Budgets.</returns>
+	public async Task<PagedList<BudgetTransactionInfo>> Handle(GetBudgetTransactions query, CancellationToken cancellationToken)
+	{
+		var budgets = this.budgetDbContext.Transaction.AsQueryable()
+			.For(query.BudgetId)
+			.WithType(query.TransactionType);
 
-        int totalItemsCount = await budgets.CountAsync(cancellationToken: cancellationToken);
-        var mappedData = await budgets
-            .OrderByDescending(x => x.BudgetTransactionDate)
-            .Paginate(query)
-            .MapToTransactionInfo()
-            .ToListAsync(cancellationToken: cancellationToken);
-        var result = new PagedList<BudgetTransactionInfo> { Items = mappedData, TotalCount = totalItemsCount };
-        return result;
-    }
+		int totalItemsCount = await budgets.CountAsync(cancellationToken: cancellationToken);
+		var mappedData = await budgets
+			.OrderByDescending(x => x.BudgetTransactionDate)
+			.Paginate(query)
+			.MapToTransactionInfo()
+			.ToListAsync(cancellationToken: cancellationToken);
+		var result = new PagedList<BudgetTransactionInfo> { Items = mappedData, TotalCount = totalItemsCount };
+		return result;
+	}
 }
