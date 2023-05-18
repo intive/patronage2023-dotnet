db5a1d66f38d63263ae75bc3b081c37b2d8ff11e
AlanP <a.pawski.ap@gmail.com>
2023-05-19 00:53:20 +0200

    Add missing files from previous commit.

--- src/modules/budget/application/Budget/AddingBudgetTransactionAttachment/AddingBudgetTransactionAttachment.cs
+++ src/modules/budget/application/Budget/AddingBudgetTransactionAttachment/AddingBudgetTransactionAttachment.cs
@@ -1,6 +1,63 @@
+using FluentValidation;
+using Intive.Patronage2023.Modules.Budget.Application.Data;
+using Intive.Patronage2023.Modules.Budget.Contracts.ValueObjects;
+using Intive.Patronage2023.Modules.Budget.Domain;
+using Intive.Patronage2023.Modules.Budget.Infrastructure.Domain;
+using Intive.Patronage2023.Shared.Abstractions.Commands;
+using Microsoft.AspNetCore.Http;
+
 namespace Intive.Patronage2023.Modules.Budget.Application.Budget.AddingBudgetTransactionAttachment;
 
-public class AddingBudgetTransactionAttachment
+/// <summary>
+/// Command adding budget transaction attachment.
+/// </summary>
+/// <param name="File">File.</param>
+public record AddingBudgetTransactionAttachment(IFormFile File, TransactionId TransactionId) : ICommand;
+
+/// <summary>
+/// Method that handles adding attachment to budget transaction.
+/// </summary>
+public class HandleAddingBudgetTransactionAttachment : ICommandHandler<AddingBudgetTransactionAttachment>
 {
-	
+	private readonly IBlobStorageService blobStorageService;
+	private readonly AddingBudgetTransactionAttachmentValidator attachmentValidator;
+	private readonly BudgetTransactionRepository budgetTransactionRepository;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="HandleAddingBudgetTransactionAttachment"/> class.
+	/// Constructor.
+	/// </summary>
+	/// <param name="blobStorageService">Blob storage service.</param>
+	/// <param name="attachmentValidator">Attachment validator.</param>
+	/// <param name="budgetTransactionRepository">Budget transaction repository.</param>
+	public HandleAddingBudgetTransactionAttachment(IBlobStorageService blobStorageService, AddingBudgetTransactionAttachmentValidator attachmentValidator, BudgetTransactionRepository budgetTransactionRepository)
+	{
+		this.blobStorageService = blobStorageService;
+		this.attachmentValidator = attachmentValidator;
+		this.budgetTransactionRepository = budgetTransactionRepository;
+	}
+
+	/// <inheritdoc/>
+	public async Task Handle(AddingBudgetTransactionAttachment command, CancellationToken cancellationToken)
+	{
+		var file = command.File;
+
+		var validationResult = this.attachmentValidator.Validate(file);
+		if (!validationResult.IsValid)
+		{
+			throw new ValidationException(validationResult.Errors);
+		}
+
+		var attachmentFile = new BudgetTransactionAttachmentModel()
+		{
+			FileName = file.Name,
+			Content = file.OpenReadStream(),
+		};
+
+		string fileUrl = this.blobStorageService.UploadFileAsync("user-name", attachmentFile.FileName, attachmentFile.Content).ToString()!;
+
+		BudgetTransactionAggregate? transaction = await this.budgetTransactionRepository.GetById(command.TransactionId);
+		transaction?.AddAttachment(fileUrl);
+		await this.budgetTransactionRepository.Persist(transaction ?? throw new InvalidOperationException());
+	}
 }
--- src/modules/budget/application/Data/BlobStorageService.cs
+++ src/modules/budget/application/Data/BlobStorageService.cs
@@ -1,6 +1,39 @@
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
 namespace Intive.Patronage2023.Modules.Budget.Application.Data;
 
-public class BlobStorageService
+/// <summary>
+/// Azure Blob Storage Service class.
+/// </summary>
+public class BlobStorageService : IBlobStorageService
 {
-	
+	private readonly BlobServiceClient blobServiceClient;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BlobStorageService"/> class.
+	/// Blob storage service.
+	/// </summary>
+	/// <param name="configuration">IConfiguration.</param>
+	public BlobStorageService(IConfiguration configuration)
+	{
+		this.blobServiceClient = new BlobServiceClient(configuration.GetConnectionString("BlobStorage"));
+	}
+
+	/// <summary>
+	/// Asynchronously uploading file to Azure Blob Storage.
+	/// </summary>
+	/// <param name="containerName">Name of container.</param>
+	/// <param name="fileName">File name.</param>
+	/// <param name="fileData">File content.</param>
+	/// <returns>Url to uploaded file.</returns>
+	public async Task<string> UploadFileAsync(string containerName, string fileName, Stream fileData)
+	{
+		BlobContainerClient blobContainerClient = this.blobServiceClient.GetBlobContainerClient(containerName);
+		BlobClient blobClient = blobContainerClient.GetBlobClient(fileName);
+
+		await blobClient.UploadAsync(fileData);
+
+		return blobClient.Uri.ToString();
+	}
 }
