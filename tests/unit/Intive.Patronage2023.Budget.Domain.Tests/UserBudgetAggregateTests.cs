54efad5a665d7568455467a4f730dbdb327e67cf
DawidBski <dawibski@gmail.com>
2023-05-19 08:38:54 +0200

    [PATRO23-337] remove unnecessary getter tests.

--- tests/unit/Intive.Patronage2023.Budget.Domain.Tests/UserBudgetAggregateTests.cs
+++ tests/unit/Intive.Patronage2023.Budget.Domain.Tests/UserBudgetAggregateTests.cs
@@ -47,7 +47,7 @@ public class UserBudgetAggregateTests
 	/// User Budget aggregate create method test with empty guid Id.
 	/// </summary>
 	[Fact]
-	public void Property_WhenPassedEmptyId_ShouldThrowInvalidOperatorExeption()
+	public void Create_WhenPassedEmptyId_ShouldThrowInvalidOperatorExeption()
 	{
 		// Arrange
 		var id = Guid.Empty;
@@ -60,81 +60,5 @@ public class UserBudgetAggregateTests
 
 		// Assert
 		act.Should().Throw<InvalidOperationException>();
-	}
-	
-	/// <summary>
-	///  This test verifies that the getter for the "Id" property of the UserBudgetAggregate class returns the correct Id.
-	/// </summary>
-	[Fact]
-	public void Property_WhenTryToGetIdProperty_ShouldReturnCorrectId()
-	{
-		// Arrange
-		var id = Guid.NewGuid();
-		var userId = new UserId(Guid.NewGuid());
-		var budgetId = new BudgetId(Guid.NewGuid());
-		var userRole = UserRole.BudgetOwner;
-		
-		//Act
-		var instance = UserBudgetAggregate.Create(id, userId, budgetId, userRole);
-		
-		//Assert
-		Assert.Equal(instance.Id, id);
-	}
-	
-	/// <summary>
-	///  This test verifies that the getter for the "UserId" property of the UserBudgetAggregate class returns the correct UserId.
-	/// </summary>
-	[Fact]
-	public void Property_WhenTryToGetUserIdProperty_ShouldReturnCorrectUserId()
-	{
-		// Arrange
-		var id = Guid.NewGuid();
-		var userId = new UserId(Guid.NewGuid());
-		var budgetId = new BudgetId(Guid.NewGuid());
-		var userRole = UserRole.BudgetOwner;
-		
-		//Act
-		var instance = UserBudgetAggregate.Create(id, userId, budgetId, userRole);
-		
-		//Assert
-		Assert.Equal(instance.UserId, userId);
-	}
-	
-	/// <summary>
-	///  This test verifies that the getter for the "BudgetId" property of the UserBudgetAggregate class returns the correct BudgetId.
-	/// </summary>
-	[Fact]
-	public void Property_WhenTryToGetBudgetIdProperty_ShouldReturnCorrectBudgetId()
-	{
-		// Arrange
-		var id = Guid.NewGuid();
-		var userId = new UserId(Guid.NewGuid());
-		var budgetId = new BudgetId(Guid.NewGuid());
-		var userRole = UserRole.BudgetOwner;
-		
-		//Act
-		var instance = UserBudgetAggregate.Create(id, userId, budgetId, userRole);
-		
-		//Assert
-		Assert.Equal(instance.BudgetId, budgetId);
-	}
-	
-	/// <summary>
-	///  This test verifies that the getter for the "UserRole" property of the UserBudgetAggregate class returns the correct UserRole.
-	/// </summary>
-	[Fact]
-	public void Property_WhenTryToGetUserRoleProperty_ShouldReturnCorrectUserRole()
-	{
-		// Arrange
-		var id = Guid.NewGuid();
-		var userId = new UserId(Guid.NewGuid());
-		var budgetId = new BudgetId(Guid.NewGuid());
-		var userRole = UserRole.BudgetOwner;
-		
-		//Act
-		var instance = UserBudgetAggregate.Create(id, userId, budgetId, userRole);
-		
-		//Assert
-		Assert.Equal(instance.UserRole, userRole);
 	}
 }
