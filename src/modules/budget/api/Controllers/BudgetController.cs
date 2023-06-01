8468b73557b430bea18f7dd4e7e750574179873d
Piotr Paluszek <piotr.paluszek@nokia.com>
2023-06-01 19:04:27 +0200

    [PATRO23-313] Updated GetTransactionByBudgetId endpoint remarks

--- src/modules/budget/api/Controllers/BudgetController.cs
+++ src/modules/budget/api/Controllers/BudgetController.cs
@@ -398,6 +398,7 @@ public class BudgetController : ControllerBase
 	/// Sample request:
 	/// Types: "Income", "Expense"
 	/// Categories: "HomeSpendings" ,  "Subscriptions" , "Car" , "Grocery" ,
+	/// SortDescriptors columns: 0 -  Name, 1 - CategoryType, 2 - Status, 3 - Value, 4 - BudgetTransactionDate, 5 - Username
 	/// Set transactionType to null or don't include at all to get both types. Same with categoryTypes.
 	///
 	///     {
@@ -408,7 +409,17 @@ public class BudgetController : ControllerBase
 	///           "HomeSpendings",
 	///           "Car"
 	///         ]
-	///         "search": "text"
+	///         "search": "text",
+	///         "sortDescriptors": [
+	///         {
+	///             "column": 3,
+	///             "sortAscending": false
+	///         },
+	///         {
+	///             "column": 0,
+	///             "sortAscending": true
+	///         }
+	///       ]
 	///     }
 	/// .</remarks>
 	/// <response code="200">Returns the list of Budget details, list of incomes and Expenses corresponding to the query.</response>
