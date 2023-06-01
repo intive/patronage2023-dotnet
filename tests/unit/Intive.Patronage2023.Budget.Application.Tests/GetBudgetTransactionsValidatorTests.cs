73a1ff7b0d54bd376a1721d8773a3f2807d7084c
Piotr Paluszek <piotr.paluszek@nokia.com>
2023-06-01 19:28:31 +0200

    [PATRO23-313] GetBudgetTransactionsValidatorTests fixed

--- tests/unit/Intive.Patronage2023.Budget.Application.Tests/GetBudgetTransactionsValidatorTests.cs
+++ tests/unit/Intive.Patronage2023.Budget.Application.Tests/GetBudgetTransactionsValidatorTests.cs
@@ -42,12 +42,17 @@ public class GetBudgetTransactionsValidatorTests
 		var budgetId = new BudgetId(new Faker().Random.Guid()); //TODO: It must not be existing BudgetId in database.
 		int pageIndex = 1;
 		int pageSize = new Faker().Random.Number(1, 10);
+		var sortDescriptors = new List<TransactionSortDescriptor>() { new TransactionSortDescriptor { Column = 0, SortAscending = true }, new TransactionSortDescriptor { Column = 3, SortAscending = false } };
 
 		var getBudgetTransactions = new GetBudgetTransactions
 		{
 			PageSize = pageSize,
 			PageIndex = pageIndex,
+			TransactionType = null,
+			CategoryTypes = null,
+			Search = string.Empty,
 			BudgetId = budgetId,
+			SortDescriptors = sortDescriptors,
 		};
 
 		//Act
@@ -67,12 +72,17 @@ public class GetBudgetTransactionsValidatorTests
 		var budgetId = new BudgetId(new Faker().Random.Guid()); //TODO: It must be existing BudgetId in database.
 		int pageIndex = 0;
 		int pageSize = 0;
+		var sortDescriptors = new List<TransactionSortDescriptor>() { new TransactionSortDescriptor { Column = 0, SortAscending = true }, new TransactionSortDescriptor { Column = 3, SortAscending = false } };
 
 		var getBudgetTransactions = new GetBudgetTransactions
 		{
 			PageSize = pageSize,
 			PageIndex = pageIndex,
+			TransactionType = null,
+			CategoryTypes = null,
+			Search = string.Empty,
 			BudgetId = budgetId,
+			SortDescriptors = sortDescriptors,
 		};
 
 		//Act
@@ -102,12 +112,17 @@ public class GetBudgetTransactionsValidatorTests
 		string? description = new Faker().Random.String(1, 10);
 		var budget = BudgetAggregate.Create(budgetId, name, userId, limit, period, icon, description);
 		this.budgetRepositoryMock.Setup(x => x.GetById(It.IsAny<BudgetId>())).ReturnsAsync(budget);
+		var sortDescriptors = new List<TransactionSortDescriptor>() { new TransactionSortDescriptor { Column = 0, SortAscending = true }, new TransactionSortDescriptor { Column = 3, SortAscending = false } };
 
 		var getBudgetTransactions = new GetBudgetTransactions
 		{
 			PageSize = pageSize,
 			PageIndex = pageIndex,
+			TransactionType = null,
+			CategoryTypes = null,
+			Search = string.Empty,
 			BudgetId = budgetId,
+			SortDescriptors = sortDescriptors,
 		};
 
 		//Act
