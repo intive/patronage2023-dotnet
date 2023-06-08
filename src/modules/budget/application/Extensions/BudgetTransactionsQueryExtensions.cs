2af443eac45ba6003776798bed0356065c9cfb8d
Piotr Paluszek <piotr.paluszek@nokia.com>
2023-06-08 16:37:38 +0200

    {PATRO23-313} Remove sort from budget transaction query extension

--- src/modules/budget/application/Extensions/BudgetTransactionsQueryExtensions.cs
+++ src/modules/budget/application/Extensions/BudgetTransactionsQueryExtensions.cs
@@ -2,8 +2,6 @@ using System.Linq.Dynamic.Core;
 using Intive.Patronage2023.Modules.Budget.Contracts.TransactionEnums;
 using Intive.Patronage2023.Modules.Budget.Contracts.ValueObjects;
 using Intive.Patronage2023.Modules.Budget.Domain;
-using Intive.Patronage2023.Shared.Abstractions;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Intive.Patronage2023.Modules.Budget.Application.Extensions;
 
@@ -65,39 +63,5 @@ internal static class BudgetTransactionsQueryExtensions
 	public static IQueryable<BudgetTransactionAggregate> WithCategoryTypes(this IQueryable<BudgetTransactionAggregate> query, CategoryType[]? categoryTypes)
 	{
 		return categoryTypes is null || !categoryTypes.Any() ? query : query.Where(x => categoryTypes.Contains(x.CategoryType));
-	}
-
-	/// <summary>
-	/// Sorting extension method.
-	/// </summary>
-	/// <typeparam name="T">Type of queryable.</typeparam>
-	/// <param name="source">Query.</param>
-	/// <param name="sortDescriptors">Sort criteria.</param>
-	/// <returns>Sorted query.</returns>
-	public static IQueryable<T> Sort<T>(this IQueryable<T> source, List<SortDescriptor> sortDescriptors)
-	{
-		if (sortDescriptors.IsNullOrEmpty())
-		{
-			return source;
-		}
-
-		if (!typeof(T).GetProperties().Any(x => x.Name == sortDescriptors[0].ColumnName))
-		{
-			throw new InvalidOperationException($"Sorting on column {sortDescriptors[0].ColumnName} is not allowed.");
-		}
-
-		var query = sortDescriptors[0].SortAscending ? source.OrderBy(sortDescriptors[0].ColumnName) : source.OrderBy(sortDescriptors[0].ColumnName + " desc");
-
-		foreach (var sortDescriptor in sortDescriptors.Skip(1))
-		{
-			if (!typeof(T).GetProperties().Any(x => x.Name == sortDescriptor.ColumnName))
-			{
-				throw new InvalidOperationException($"Sorting on column {sortDescriptor.ColumnName} is not allowed.");
-			}
-
-			query = sortDescriptor.SortAscending ? query.ThenBy(sortDescriptor.ColumnName) : query.ThenBy(sortDescriptor.ColumnName + " desc");
-		}
-
-		return query;
 	}
 }
