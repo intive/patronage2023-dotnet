b4bd20daad5b5e1e335538cd73404766a08b1cdc
Michał Szczepaniak <miher13@wp.pl>
2023-06-09 17:50:38 +0200

    [PATRO23-412] fix access token.

--- src/modules/user/application/RefreshingUserToken/RefreshUserToken.cs
+++ src/modules/user/application/RefreshingUserToken/RefreshUserToken.cs
@@ -2,6 +2,7 @@ using Intive.Patronage2023.Modules.User.Application.Extensions;
 using Intive.Patronage2023.Modules.User.Infrastructure;
 using Intive.Patronage2023.Shared.Abstractions.Queries;
 using Intive.Patronage2023.Shared.Infrastructure.Exceptions;
+using Microsoft.AspNetCore.Http;
 
 namespace Intive.Patronage2023.Modules.User.Application.RefreshingUserToken;
 /// <summary>
@@ -21,18 +22,25 @@ public record AccessUserToken(string AccessToken);
 /// </summary>
 public class HandleRefreshUserToken : IQueryHandler<RefreshUserToken, AccessUserToken>
 {
+	private readonly IHttpContextAccessor httpContextAccessor;
 	private readonly IKeycloakService keycloakService;
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="HandleRefreshUserToken"/> class.
 	/// </summary>
-	/// <param name="keycloakService">KeycloakService.</param>
-	public HandleRefreshUserToken(IKeycloakService keycloakService) => this.keycloakService = keycloakService;
+	/// <param name="keycloakService">.</param>
+	/// <param name="httpContextAccessor">KeycloakService.</param>
+	public HandleRefreshUserToken(IKeycloakService keycloakService, IHttpContextAccessor httpContextAccessor)
+	{
+		this.keycloakService = keycloakService;
+		this.httpContextAccessor = httpContextAccessor;
+	}
 
 	/// <inheritdoc/>
 	public async Task<AccessUserToken> Handle(RefreshUserToken command, CancellationToken cancellationToken)
 	{
-		string oldAccessToken = await this.keycloakService.ExtractAccessTokenFromClientToken(cancellationToken);
+		string? oldAccessToken = this.httpContextAccessor.HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
 		string response = string.Empty;
 
 		if (command.RefreshToken.IsExpired())
@@ -40,7 +48,7 @@ public class HandleRefreshUserToken : IQueryHandler<RefreshUserToken, AccessUser
 			throw new AppException("refresh token is expired.");
 		}
 
-		if (oldAccessToken.IsExpired(TimeSpan.FromMinutes(-1)))
+		if (oldAccessToken!.IsExpired(TimeSpan.FromMinutes(-1)))
 		{
 			response = await this.keycloakService.RefreshUserToken(command.RefreshToken, cancellationToken);
 		}
