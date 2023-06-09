f7176f93fb4e253c6c95fb054f4691d19f576c7b
KlaudiaGajocha <klaudiagajocha99@gmail.com>
2023-06-09 08:42:44 +0200

    fix test

--- tests/integration/Intive.Patronage2023.Shared.IntegrationTests/Email/EmailSendingTests.cs
+++ tests/integration/Intive.Patronage2023.Shared.IntegrationTests/Email/EmailSendingTests.cs
@@ -1,58 +1,50 @@
-//using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Options;
 
-//using Intive.Patronage2023.Shared.Infrastructure.Email;
+using Intive.Patronage2023.Shared.Infrastructure.Email;
 
-//using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using FluentAssertions;
 
-//namespace Intive.Patronage2023.Shared.IntegrationTests.Email;
+namespace Intive.Patronage2023.Shared.IntegrationTests.Email;
 
-///// <summary>
-///// Class that contains email sending service integration tests.
-///// </summary>
-//public class EmailSendingTests : AbstractIntegrationTests, IClassFixture<SmtpServerFixture>
-//{
-//	private readonly SmtpServerFixture fixture;
-//	private IEmailService emailService;
-//	private IEmailConfiguration emailConfiguration;
+/// <summary>
+/// Class that contains email sending service integration tests.
+/// </summary>
+public class EmailSendingTests : AbstractIntegrationTests, IClassFixture<SmtpServerFixture>
+{
+	private readonly SmtpServerFixture fixture;
+	private IEmailService emailService;
+	private IEmailConfiguration emailConfiguration;
 
-//	/// <summary>
-//	/// Initializes email tests.
-//	/// </summary>
-//	/// <param name="fixture">Email fixture.</param>
-//	/// <param name="dbFixture">Email fixture.</param>
-//	public EmailSendingTests(SmtpServerFixture fixture, MsSqlTests dbFixture) : base(dbFixture)
-//	{
-//		this.fixture = fixture;
-//		var scope = this.WebApplicationFactory.Services.CreateScope();
-//		this.emailConfiguration = scope.ServiceProvider.GetRequiredService<IOptions<IEmailConfiguration>>().Value;
-//		this.emailService = scope.ServiceProvider.GetRequiredService<IEmailService>();
-//	}
+	/// <summary>
+	/// Initializes email tests.
+	/// </summary>
+	/// <param name="fixture">Email fixture.</param>
+	/// <param name="dbFixture">Email fixture.</param>
+	public EmailSendingTests(SmtpServerFixture fixture, MsSqlTests dbFixture) : base(dbFixture)
+	{
+		this.fixture = fixture;
+		var scope = this.WebApplicationFactory.Services.CreateScope();
+		this.emailConfiguration = scope.ServiceProvider.GetRequiredService<IOptions<IEmailConfiguration>>().Value;
+		this.emailService = scope.ServiceProvider.GetRequiredService<IEmailService>();
+	}
 
-	/////// <summary>
-	/////// Integration test that verifes if email service does not throw exeptions when called properly.
-	/////// </summary>
-	/////// <returns>Test.</returns>
-	////[Fact]
-	////public void SendEmail_WhenCalledProperly_ShouldNotThrowExceptions()
-	////{
-	////	string attachmentContent = "test";
-	////	byte[] attachmentContentBytes = Encoding.UTF8.GetBytes(attachmentContent);
-	////	string attachmentContentBase64 = Convert.ToBase64String(attachmentContentBytes);
+	/// <summary>
+	/// Integration test that verifes if email service does not throw exeptions when called properly.
+	/// </summary>
+	/// <returns>Test.</returns>
+	[Fact]
+	public void SendEmail_WhenCalledProperly_ShouldNotThrowExceptions()
+	{
+		var emailMessage = new EmailMessage
+		{
+			Subject = "Test subject",
+			Body = "Test body",
+			SendFromAddress = new EmailAddress("testFrom", "testFrom@intive.pl"),
+			SendToAddresses = new List<EmailAddress> { new("testTo", "testTo@invite.pl") },
+		};
 
-	////	var emailMessage = new EmailMessage
-	////	{
-	////		Subject = "Test subject",
-	////		Body = "Test body",
-	////		SendFromAddress = new EmailAddress("testFrom", "testFrom@intive.pl"),
-	////		SendToAddresses = new List<EmailAddress> { new EmailAddress("testTo", "testTo@invite.pl") },
-	////		EmailAttachments = new List<EmailAttachment>
-	////	{
-	////		new EmailAttachment("attachment.csv", attachmentContentBase64)
-	////	}
-	////	};
-
-	////	this.emailService.Invoking(service => service.SendEmail(emailMessage))
-//	////		.Should().NotThrow();
-//	////}
-
-//}
+		this.emailService.Invoking(service => service.SendEmail(emailMessage))
+				.Should().NotThrow();
+	}
+}
